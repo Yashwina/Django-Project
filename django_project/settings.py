20d95940153fc9d9192c9a71bcaf19fcf6e4dd0c
Yashwina <yashwina.ramamoorthy@gmail.com>
2019-10-30 21:19:23 +0000

    update

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4m=r62&9yl=49b#k8#8ru*(an1vh*kppw6^sqk$r63##p!(l)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sleepy-mesa-02501.herokuapp.com/']
 
 
 # Application definition
