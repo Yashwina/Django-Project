f682f1ce348ed8d3cc18126148c4d7ef8d558468
Yashwina <yashwina.ramamoorthy@gmail.com>
2019-10-30 20:56:30 +0000

    COMMIT

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4m=r62&9yl=49b#k8#8ru*(an1vh*kppw6^sqk$r63##p!(l)s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
