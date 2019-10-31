a8e3e474d0602498b82b7b432ba8efe9a040125a
Yashwina <yashwina.ramamoorthy@gmail.com>
2019-10-31 11:40:52 +0000

    added

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': dj_database_url.config(conn_max_age=600,ssl_require=True)
     }
 }
 
